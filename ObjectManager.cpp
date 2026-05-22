fff10165221dc8b9d6625cac90ab41fd6a506030
ShojiMakotoX <250039@jc-21.jp>
2026-05-23 00:00:24 +0900

    プロジェクトファイルを追加。

---
+++ ObjectManager.cpp
@@ -0,0 +1,17 @@
+#include "ObjectManager.h"
+
+void ObjectManager::Initlaize()
+{
+}
+
+void ObjectManager::Update()
+{
+}
+
+void ObjectManager::Draw()
+{
+}
+
+void ObjectManager::Release()
+{
+}
---
+++ ObjectManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "DxLib.h"
+
+class ObjectManager
+{
+public:
+	void Initlaize();
+	void Update();
+	void Draw();
+	void Release();
+private:
+
+};
